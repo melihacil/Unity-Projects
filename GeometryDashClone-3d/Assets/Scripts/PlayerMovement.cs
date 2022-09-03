2c439009e0090f4d9a14b047958222b0e410fa01
melihacil <knghtmellomello@gmail.com>
2022-09-03 12:48:09 +0300

    Update PlayerMovement.cs

--- GeometryDashClone-3d/Assets/Scripts/PlayerMovement.cs
+++ GeometryDashClone-3d/Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,27 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField] float horizontalInput = 1f;
+    [SerializeField] LayerMask groundLayer;
+    private float gravity = -50f;
+
+    private Vector3 velocity;
+
+    private CharacterController characterController;
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //Adding gravity to velocity
+        velocity.y += gravity * Time.deltaTime;
+
+
+        //Using component move with velocity
+        characterController.Move(velocity * Time.deltaTime);
+
     }
 }

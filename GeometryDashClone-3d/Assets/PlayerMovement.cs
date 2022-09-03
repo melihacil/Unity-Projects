27cd4bb09f90f2b7b4a949b67de6ceb7da8a6a14
melihacil <knghtmellomello@gmail.com>
2022-09-03 12:35:16 +0300

    Adding player

---
+++ GeometryDashClone-3d/Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] float horizontalInput = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

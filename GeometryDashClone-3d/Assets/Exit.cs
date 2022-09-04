f73def9ee5356beb9616f84d676fa68bdaabf2f0
melihacil <knghtmellomello@gmail.com>
2022-09-04 11:15:40 +0300

    Adding exit block

---
+++ GeometryDashClone-3d/Assets/Exit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().LoadNextLevel();
+        }
+    }
+}

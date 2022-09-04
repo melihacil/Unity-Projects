4abdbeb2517af1ac793fbaccc4019fef4ece303d
melihacil <knghtmellomello@gmail.com>
2022-09-04 10:52:15 +0300

    Update RandomTrails.cs

--- GeometryDashClone-3d/Assets/Scripts/RandomTrails.cs
+++ GeometryDashClone-3d/Assets/Scripts/RandomTrails.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class RandomTrails : MonoBehaviour
 {
+
+    private TrailRenderer trailRenderer;
+    private float duration;
+    private float timeStamp;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        trailRenderer = GetComponent<TrailRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > timeStamp + duration)
+        {
+            duration = Random.Range(0.05f, 0.3f);
+            timeStamp = Time.time;
+            trailRenderer.emitting = !trailRenderer.emitting;
+        }
     }
 }
